aaf8746cb5b01509c889d73e4eddc9d2d94d9d53
hiago silva <hiago3259@gmail.com>
2026-05-12 14:43:50 -0300

    feat: criando as variaveis

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+const produtos = [
+    {
+        nome:"Spider-man",
+        preco: 299.90,
+        imagem: ".src/assets/img/spiderman.webp"
+    },
+    {
+        nome:"Batman",
+        preco: 159.99,
+        imagem: ".src/assets/img/batman.webp"
+    },
+    {
+        nome:"Iron-man",
+        preco: 159.99,
+        imagem: ".src/assets/img/ironman.webp"
+    },
+    {
+        nome:"Superman",
+        preco: 159.99,
+        imagem: ".src/assets/img/superman.webp"
+    },
+    
+];
+const secaoProdutos = document.getElementById("produtos");
